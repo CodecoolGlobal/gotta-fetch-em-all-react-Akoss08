708e9ff10ff4e94273954af89d1d323718902171
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-05-22 09:29:41 +0200

    implement pokemon fetching by area

--- pokemon-project/src/components/Location.jsx
+++ pokemon-project/src/components/Location.jsx
@@ -1,26 +1,34 @@
 function Location(location) {
-  async function getPokemonsByArea(url, setIsTherePokemon, setIsLocationClicked) {
-    setIsLocationClicked(true);
-    setIsTherePokemon(true);
-    let randomIndex = 0;
-    const locationResponse = await fetch(url);
-    const location = await locationResponse.json();
+  async function getEnemyPokemonByArea(url, setIsLocationClicked, setCurrentEnemyPokemon) {
+    try {
+      let randomIndex = 0;
+      const locationResponse = await fetch(url);
+      const location = await locationResponse.json();
 
-    if (location.areas.length > 0) {
-      randomIndex = Math.floor(Math.random() * location.areas.length);
-    } else {
-      setIsTherePokemon(false);
-      return;
+      if (location.areas.length > 0) {
+        randomIndex = Math.floor(Math.random() * location.areas.length);
+        const areaResponse = await fetch(location.areas[randomIndex].url);
+        const area = await areaResponse.json();
+
+        const pokemonResponse = await fetch(area['pokemon_encounters'][Math.floor(Math.random() * area['pokemon_encounters'].length)].pokemon.url);
+        const pokemon = await pokemonResponse.json();
+        console.log(pokemon);
+        setCurrentEnemyPokemon(pokemon);
+      } else {
+        setCurrentEnemyPokemon(null);
+      }
+    } catch (err) {
+      console.error(`Error fetching from ${url}`);
+    } finally {
+      setIsLocationClicked(true);
     }
-
-    const areaResponse = await fetch(location.areas[randomIndex].url);
-    const area = await areaResponse.json();
-
-    const pokemonResponse = await fetch(area['pokemon_encounters'][Math.floor(Math.random() * area['pokemon_encounters'].length)].pokemon.url);
-    const pokemon = await pokemonResponse.json();
   }
 
-  return <h2 onClick={() => getPokemonsByArea(location.url, location.setIsTherePokemon, location.setIsLocationClicked)}>{location.name}</h2>;
+  return (
+    <>
+      <h2 onClick={() => getEnemyPokemonByArea(location.url, location.setIsLocationClicked, location.setCurrentEnemyPokemon)}>{location.name}</h2>
+    </>
+  );
 }
 
 export default Location;
