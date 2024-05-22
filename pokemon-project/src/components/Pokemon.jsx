e4b686293003e9e29c3cb8119a5b0179830feb14
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-05-22 09:30:20 +0200

    implement render pokemon informations

---
+++ pokemon-project/src/components/Pokemon.jsx
@@ -0,0 +1,16 @@
+import ProgressBar from './ProgressBar';
+
+function Pokemon(pokemon) {
+  return (
+    <div className="pokemonCard">
+      <h2 id="pokemonCardName">{pokemon.pokemonName}</h2>
+      <img src={pokemon.img}></img>
+      {pokemon.stats.map((stat, index) => (
+        <ProgressBar key={index} value={stat['base_stat']} name={stat.stat.name}></ProgressBar>
+      ))}
+      <button onClick={pokemon.handleBackClick}>Back</button>
+    </div>
+  );
+}
+
+export default Pokemon;
