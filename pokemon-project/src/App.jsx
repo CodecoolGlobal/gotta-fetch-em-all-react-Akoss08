ded684d25a954bd87e97d10ffbc590eb63c3f940
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-05-22 09:28:20 +0200

    implement renderPokemon && renderLocation

--- pokemon-project/src/App.jsx
+++ pokemon-project/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import Location from './components/Location';
+import Pokemon from './components/Pokemon';
 
 function App() {
   const [locations, setLocation] = useState(null);
-  const [isTherePokemon, setIsTherePokemon] = useState(true);
   const [isLocationClicked, setIsLocationClicked] = useState(false);
+  const [currentEnemyPokemon, setCurrentEnemyPokemon] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -20,23 +21,50 @@ function App() {
     fetchData();
   }, []);
 
-  return !isLocationClicked ? (
-    <div className="LocationContainer">
-      {locations &&
-        locations.map((location, index) => (
-          <Location key={index} name={location.name} url={location.url} setIsTherePokemon={setIsTherePokemon} setIsLocationClicked={setIsLocationClicked}></Location>
-        ))}
-    </div>
-  ) : isTherePokemon ? (
-    <div>
-      <h2>There are pokemons here</h2>
-    </div>
-  ) : (
-    <div>
-      <h2>There are no pokemons here</h2>
-      <button onClick={() => setIsLocationClicked(false)}>Back</button>
-    </div>
-  );
+  const handleBackClick = () => {
+    setIsLocationClicked(false);
+  };
+
+  function renderLocations() {
+    return (
+      <div className="LocationContainer">
+        {
+          locations?.map((location, index) => (
+            <Location key={index} name={location.name} url={location.url} setIsLocationClicked={setIsLocationClicked} setCurrentEnemyPokemon={setCurrentEnemyPokemon} />
+          ))}
+      </div>
+    );
+  }
+
+  function renderPokemon() {
+    if (currentEnemyPokemon) {
+      return (
+        <>
+          {currentEnemyPokemon && (
+            <Pokemon
+              img={currentEnemyPokemon.sprites.other['official-artwork']['front_default']}
+              stats={currentEnemyPokemon.stats}
+              pokemonName={currentEnemyPokemon.name}
+              handleBackClick={handleBackClick}
+            />
+          )}
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2>There are no pokemons here</h2>
+          <button onClick={handleBackClick}>Back</button>
+        </>
+      );
+    }
+  }
+
+  if (isLocationClicked) {
+    return renderPokemon();
+  } else {
+    return renderLocations();
+  }
 }
 
 export default App;
