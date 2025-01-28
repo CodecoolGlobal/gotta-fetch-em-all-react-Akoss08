19f8d57a46583600f62857b8b6173fd0d7547a19
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-01-28 12:31:25 +0100

    implement pagination component

---
+++ pokemon-project/src/components/Pagination.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
+function Pagination({ offset, onNextClick, onPreviousClick, isLastPage }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="pokedexButtonContainer">
+      <button className="pokedexButton" onClick={onPreviousClick} disabled={offset < 1}>
+        Previous page
+      </button>
+      <button className="pokedexButton" onClick={() => navigate('/')}>
+        Home
+      </button>
+      <button className="pokedexButton" onClick={onNextClick} disabled={isLastPage()}>
+        Next page
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
