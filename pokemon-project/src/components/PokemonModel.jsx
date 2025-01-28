6e46032a22469479a473b045bb9e6c0839457838
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-01-28 12:30:10 +0100

    design depends on css, instead of logic

--- pokemon-project/src/components/PokemonModel.jsx
+++ pokemon-project/src/components/PokemonModel.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react';
 
 function PokemonModel({ sprite, isLost, baseClass, lostBattleClass }) {
-  const [isVisible, setIsVisible] = useState(true);
-
   const className = isLost ? `${baseClass} ${lostBattleClass}` : `${baseClass}`;
-
-  if (!isVisible) return null;
 
   return (
     <>
-      <img src={sprite} className={className} onAnimationEnd={() => setIsVisible(false)}></img>
+      <img src={sprite} className={className}></img>
     </>
   );
 }
